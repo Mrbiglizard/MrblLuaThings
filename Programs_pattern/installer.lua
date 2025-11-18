01b8c9d437bed68484d25443f450c8c1324935bb
Mrbiglizard <98935438+Mrbiglizard@users.noreply.github.com>
2025-11-18 15:55:08 +0500

    Update installer.lua

--- Programs_pattern/installer.lua
+++ Programs_pattern/installer.lua
@@ -1,4 +1,26 @@
-print("I'm installer! I install example!")
-print("Let's run this!")
-shell.run("ExPr/example.lua") 
+arg = { ... }
 
+local function update()
+    error("I can not be update by installer!")
+end
+
+local function install()
+    print("I'm installer! I install example!")
+    print("Let's run this!")
+    shell.run("ExPr/example.lua")
+end
+
+local function delete()
+    error("I can not be deleted by installer! Do this manual!")
+end
+
+if #arg <= 0 then
+    install()
+end
+if #arg >= 1 and arg[1] == "install" then
+    install()
+elseif #arg >= 1 and arg[1] == "update" then
+    update()
+elseif #arg >= 1 and arg[1] == "delete" then
+    delete()
+end
